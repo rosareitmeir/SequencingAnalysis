66a512331b404f81092dda95aa9778ae47ff710e
Rosa Reitmeir <71329291+rosareitmeir@users.noreply.github.com>
2023-07-06 17:01:15 +0200

    Delete workflow directory

--- workflow/scripts/avg_cov.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_table(snakemake.input[0], header=None)
-avg_cov = round(data.iloc[:, 2] / (data.iloc[:, 1] / 1000), 3) #truncated to 3 decimals
-data.loc[:,4] =  avg_cov
-data.to_csv(snakemake.output[0], sep="\t", header=None)
