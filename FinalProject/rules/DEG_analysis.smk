79fd6d4cb7e7d01ea3c622e31767be9381addbdd
rosareitmeir <ge72few@mytum.de>
2023-07-17 21:19:14 +0200

    changes DEG: only deseq2

--- FinalProject/rules/DEG_analysis.smk
+++ FinalProject/rules/DEG_analysis.smk
@@ -1,15 +1,16 @@
 
 rule differential_gene_expression:
         input:
-            counts= "results/featurecounts/allsamples.featureCounts",
-            group_assignment= config["group_assignments"]
+            counts= "example/allsamples.featureCounts" ,# results/featurecounts/allsamples.featureCounts",
+            group_assignment= config["group_assignment"]
         output:
             table= "results/DGEAnalysis/DEG_results.tsv",
             volcano="results/DGEAnalysis/volcano_DESeq2.pdf",
             heatmap = "results/DGEAnalysis/heatmap_DESeq2.pdf",
-            venn= "results/DGEAnalysis/venn.pdf"
+            #venn= "results/DGEAnalysis/venn.pdf"
         params:
-            alpha = config["software"]["DEG"]["alpha"]
+            alpha = config["software"]["DEG"]["alpha"],
+            topn = config["software"]["DEG"]["topn"]
         log:
             "logs/DGE_Rscript.log"
         script:
--- FinalProject/scripts/DGE_analysis.R
+++ FinalProject/scripts/DGE_analysis.R
@@ -1,4 +1,3 @@
-### Differential Gene Expression Analysis
 library(dplyr)
 library(DESeq2)
 library(ggplot2)
@@ -8,27 +7,24 @@ sink(log)
 sink(log, type="message")
 
 ## Passing argumentd from command line 
-# 1 feature counts input 
-args = commandArgs(trailingOnly = TRUE)
-
 featurecount_path = snakemake@input[['counts']]
 group_assignment_path = snakemake@input[['group_assignment']]
 
 output_table=snakemake@output[["table"]]
 output_volcano=snakemake@output[["volcano"]]
 output_heatmap=snakemake@output[["heatmap"]]
-output_venn=snakemake@output[["venn"]]
+#output_venn=snakemake@output[["venn"]]
 
-                              
+
 alpha= snakemake@params[["alpha"]]
 
 
 
 #### Import of Data 
 # just deleting the first columns of the feature counts matrix 
-# "/home/rosa/SequencingAnalysis/FinalProject/results/featurecounts/allsamples.featureCounts"
+#featurecount_path= "/home/rosa/SequencingAnalysis/FinalProject/example/count.featureCounts"
 count_data <- read.csv2(featurecount_path, 
-                     header=T, sep="\t", blank.lines.skip = T, skip=1 )
+                        header=T, sep="\t", blank.lines.skip = T, skip=1 )
 
 row.names(count_data) <- count_data$Geneid
 count_data <- count_data[, -c(1:6)]
@@ -37,125 +33,40 @@ colnames(count_data)<- gsub("results.hisat2.mapped.", "", colnames(count_data))
 colnames(count_data)<- gsub(".bam", "", colnames(count_data)) 
 
 
-###### to be deleted 
-# "/home/rosa/SequencingAnalysis/FinalProject/example/count.featureCounts"
+## importing the condition table
 
-colnames(count_data)<- gsub("results.star.pe.", "", colnames(count_data)) 
-colnames(count_data)<- gsub(".pe_aligned.sam", "", colnames(count_data)) 
-
-cond <- data.frame(sample=colnames(count_data), group=c("n", "t", "n", "t","n", "t"))
-
-condition <- cond
-
-## importing the condition table 
-# example 
 condition <- read.csv2(group_assignment_path, sep="\t", row.names = NULL)
 condition$group <- factor(condition$group)
 #dataframe(sample=colnames(example)) %>% left_join(condition, by ="sample") %>% pull(sample)
 #sort it in the order of the count matrix
-condition <- condition[match(colnames(example), condition$sample),]
+condition <- condition[match(colnames(count_data), condition$sample),]
 
-condition_vec <- condition$group
+condition_vec <- factor(condition$group)
 
 
 
 #### DESeq2
- dds <- DESeqDataSetFromMatrix(countData =count_data,
-                                 colData= condition, ~ group) 
+dds <- DESeqDataSetFromMatrix(countData =count_data,
+                              colData= condition, ~ group) 
 dds <- DESeq(dds)
-  
+
 res<- results(dds,alpha=alpha)
 
 # for volcano plot and also nice dataframe for output 
-deseq_df<- data.frame(log2FoldChange=res$log2FoldChange, padj= res$padj,pval=res$pvalue, genes=rownames(res))
+deseq_df<- data.frame(GeneID=rownames(res), log2FoldChange=res$log2FoldChange, padj= res$padj, pval=res$pvalue)
 deseq_df<- deseq_df %>%
   mutate(gene_type = case_when(log2FoldChange > 0 & padj < 0.05 ~ "upregulated",
                                log2FoldChange < 0 & padj < 0.05 ~ "downregulated",
                                TRUE ~ "not significant"))   
 
-### edgeR
+create_volcano <- function(df, method, intersection, path){
 
-library(edgeR)
-
-edge_list <- DGEList(counts=as.matrix(count_data), group=factor(group))
-
-# filter out genes with low counts
-keep <- filterByExpr(y=edge_list)
-edge_list <- edge_list[keep, , keep.lib.sizes=FALSE]
-
-# normalization / scaling 
-edge_list <- calcNormFactors(object=edge_list)
-
-# dispersion estimation
-edge_list <- estimateDisp(y=edge_list )
-
-# finally: DGE calling 
-
-edge_res <- exactTest(object=edge_list)
-
-# get adjusted p values 
-
-edge_res <- data.frame(topTags(object=edge_res, n = "Inf"))
-
-edge_df<- data_frame(log2FoldChange=edge_res$logFC, padj= edge_res$FDR,pval=edge_res$PValue, genes=rownames(edge_res))
-edge_df <- edge_df %>%
-  mutate(gene_type = case_when(log2FoldChange > 0 & padj < 0.05 ~ "upregulated",
-                               log2FoldChange < 0 & padj < 0.05 ~ "downregulated",
-                               TRUE ~ "not significant"))   
-
-
-### limma
-library(limma)
-
-limma.d <-  DGEList(counts=as.matrix(count_data), group=factor(group))
-limma.d <- calcNormFactors(limma.d)
-
-# filter out genes with low counts 
-drop <- which(apply(cpm(limma.d), 1, max) < 1)
-limma.d <- limma.d[-drop,] 
-
-mm <- model.matrix(~ group)
-voom <- voom(limma.d, mm)
-
-f1 = lmFit(voom, mm)
-ef1 = eBayes(f1)
-
-limma_res <- topTable(ef1, n=Inf)
-
-limma_df<- data_frame(log2FoldChange=limma_res$logFC, padj= limma_res$adj.P.Val,pval=limma_res$P.Value, genes=rownames(limma_res))
-limma_df <- limma_df %>%
-  mutate(gene_type = case_when(log2FoldChange > 0 & padj < 0.05 ~ "upregulated",
-                               log2FoldChange < 0 & padj < 0.05 ~ "downregulated",
-                               TRUE ~ "not significant"))   
-
-### Visualization
-
-## Venn Diagram and Intersection of the three methods 
-library(ggvenn)
-limma_signif <- filter(limma_df, gene_type != "not significant") %>% pull(genes)
-edge_signif <- filter(edge_df, gene_type != "not significant") %>% pull(genes)
-deseq_signif <- filter(deseq_df, gene_type != "not significant") %>% pull(genes)
-
-
-ggvenn(list(limma=limma_signif, edgeR=edge_signif, DESeq2=deseq_signif),
-     
-       fill_color  = c( "#E69F00", "#56B4E9", "#999999"))
-
-intersection <- deseq_df %>% filter ( genes %in% intersect(intersect(limma_signif, edge_signif), deseq_signif) )
-
-ggsave(output_venn, plot =  hm_plt, device = 'pdf')
-
-
-## create volcano plot 
-
-
-create_volcano <- function(df, path){
-  
-  return(ggplot(data=df, aes(x=log2FoldChange, y=-log10(padj) , col= gene_type  ) )+ 
+  return(ggplot(data=df, aes(x=log2FoldChange, y=-log10(padj)  , col = gene_type) )+ 
            geom_point() + 
            theme(legend.title= element_blank()) +
            xlab("log2 Fold Change") + 
            ylab("-log10( adj. p-value)"))
+  
   
 }
 
@@ -163,12 +74,10 @@ create_volcano <- function(df, path){
 volcano_plt <- create_volcano(deseq_df)
 ggsave(output_volcano, plot =  volcano_plt, device = 'pdf')
 
-
-
-## heatmap 
-
+## heatmap plot
 library("pheatmap")
-top=50
+
+top= snakemake@params[["topn"]]
 
 select <-  deseq_df %>%filter (gene_type != "not significant") %>% top_n(top, abs(log2FoldChange)) %>% pull(genes)
 
@@ -182,10 +91,13 @@ hm_plt <- pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=FALSE,
 ggsave(output_heatmap, plot =  hm_plt, device = 'pdf')
 
 
-### TODO  table output 
+
+
+
+
+
+
+
+## table output
 write.csv2(deseq_df, file= output_table, quote= F, sep = "\t", col.names =T, row.names=F)
 
-
-
-
- 
