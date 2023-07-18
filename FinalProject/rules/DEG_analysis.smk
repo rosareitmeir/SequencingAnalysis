25657cac598f870622deb5c8b52d4e9d96bb7927
rosareitmeir <ge72few@mytum.de>
2023-07-18 16:20:39 +0200

    DEG DEseq2

--- FinalProject/rules/DEG_analysis.smk
+++ FinalProject/rules/DEG_analysis.smk
@@ -5,9 +5,10 @@ rule differential_gene_expression:
             group_assignment= config["group_assignment"]
         output:
             table= "results/DGEAnalysis/DEG_results.tsv",
-            volcano="results/DGEAnalysis/volcano_DESeq2.pdf",
-            heatmap = "results/DGEAnalysis/heatmap_DESeq2.pdf",
-            #venn= "results/DGEAnalysis/venn.pdf"
+            volcano="results/DGEAnalysis/volcano.pdf",
+            heatmap = "results/DGEAnalysis/heatmap.pdf",
+            ma= "results/DGEAnalysis/MA.pdf",
+            pca="results/DGEAnalysis/PCA.pdf"
         params:
             alpha = config["software"]["DEG"]["alpha"],
             topn = config["software"]["DEG"]["topn"]
--- FinalProject/scripts/DGE_analysis.R
+++ FinalProject/scripts/DGE_analysis.R
@@ -1,10 +1,12 @@
-library(dplyr)
-library(DESeq2)
-library(ggplot2)
-
 log <- file(snakemake@log[[1]], open="wt")
 sink(log)
 sink(log, type="message")
+
+library(dplyr)
+library(DESeq2)
+library(ggplot2)
+library(pheatmap)
+
 
 ## Passing argumentd from command line 
 featurecount_path = snakemake@input[['counts']]
@@ -13,11 +15,11 @@ group_assignment_path = snakemake@input[['group_assignment']]
 output_table=snakemake@output[["table"]]
 output_volcano=snakemake@output[["volcano"]]
 output_heatmap=snakemake@output[["heatmap"]]
-#output_venn=snakemake@output[["venn"]]
-
+output_ma=snakemake@output[["ma"]]
+output_pca=snakemake@output[["pca"]]
 
 alpha= snakemake@params[["alpha"]]
-
+top= snakemake@params[["topn"]]
 
 
 #### Import of Data 
@@ -46,58 +48,95 @@ condition_vec <- factor(condition$group)
 
 
 #### DESeq2
-dds <- DESeqDataSetFromMatrix(countData =count_data,
+
+## pre filtering 
+total_count_threshold <- length(condition_vec)
+
+# Calculate the total count per gene
+total_counts <- rowSums(count_data)
+
+# Filter out genes with low counts
+filtered_counts <- count_data[total_counts >= total_count_threshold, ]
+
+
+## running deseq2
+
+dds <- DESeqDataSetFromMatrix(countData =filtered_counts,
                               colData= condition, ~ group) 
 dds <- DESeq(dds)
 
 res<- results(dds,alpha=alpha)
 
-# for volcano plot and also nice dataframe for output 
+#  dataframe for table and plotting 
 deseq_df<- data.frame(GeneID=rownames(res), log2FoldChange=res$log2FoldChange, padj= res$padj, pval=res$pvalue)
 deseq_df<- deseq_df %>%
   mutate(gene_type = case_when(log2FoldChange > 0 & padj < 0.05 ~ "upregulated",
                                log2FoldChange < 0 & padj < 0.05 ~ "downregulated",
                                TRUE ~ "not significant"))   
 
-create_volcano <- function(df, method, intersection, path){
 
-  return(ggplot(data=df, aes(x=log2FoldChange, y=-log10(padj)  , col = gene_type) )+ 
+## volcano plot 
+
+volcano_plt <- ggplot(data=deseq_df, aes(x=log2FoldChange, y= -log10(padj)  , col = gene_type) )+ 
            geom_point() + 
            theme(legend.title= element_blank()) +
            xlab("log2 Fold Change") + 
-           ylab("-log10( adj. p-value)"))
-  
-  
-}
+           ylab("-log10( adj. p-value)") +
+          xlim(-5,5)+
+          ylim(0,20)
 
 
-volcano_plt <- create_volcano(deseq_df)
-ggsave(output_volcano, plot =  volcano_plt, device = 'pdf')
+library(EnhancedVolcano)
+
+volcano <- EnhancedVolcano(deseq_df,
+                           lab = NA ,
+                           x = "log2FoldChange",
+                           y = 'padj',
+                           pCutoff = 0.05,
+                           FCcutoff = 2,
+                          # xlim = xlim,
+                          # ylim=ylim,
+                           title="", subtitle="")
+
+
+ggsave(output_volcano, plot =volcano, device = 'pdf')
+
+
+## ma plot 
+pdf(output_ma)  
+plot_ma <- plotMA(res)
+print(plot_ma)
+dev.off()
+
 
 ## heatmap plot
-library("pheatmap")
 
-top= snakemake@params[["topn"]]
-
-select <-  deseq_df %>%filter (gene_type != "not significant") %>% top_n(top, abs(log2FoldChange)) %>% pull(genes)
+select <-  deseq_df %>%filter (gene_type != "not significant") %>% top_n(top, abs(log2FoldChange)) %>% pull(GeneID)
 
 ntd<-normTransform(dds)
 
 df <- data.frame(group=colData(dds)[,c("group")])
 rownames(df) <- colnames(dds)
-hm_plt <- pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=FALSE,
-         cluster_cols=FALSE, annotation_col=df)
 
-ggsave(output_heatmap, plot =  hm_plt, device = 'pdf')
+pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
+         cluster_cols=TRUE, annotation_col=df, filename = output_heatmap)
+
+## PCA 
+vsd<- vst(dds, blind=FALSE)
+
+pcaData <- plotPCA(vsd, intgroup=c("group"), returnData=TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+pca_plt <- ggplot(pcaData, aes(PC1, PC2, color=group)) +
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) 
 
 
-
-
-
-
+ggsave(output_pca, plot =pca_plt, device = 'pdf')
 
 
 
 ## table output
-write.csv2(deseq_df, file= output_table, quote= F, sep = "\t", col.names =T, row.names=F)
+# quote= F, sep = "\t", col.names =T, row.names=F
+write.csv2(deseq_df, file= output_table, row.names = F, sep="\t")
 
